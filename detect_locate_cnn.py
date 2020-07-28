549ee50cea43fb5828f2929be6c23a6b6f429229
Matthew Gaddes <gaddes.matthew@gmail.com>
2020-07-28 17:29:49 +0100

    Nothing working - just adding files.

---
+++ detect_locate_cnn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 17:27:31 2020
+
+
+# open matlab defo data
+
+# make synthetic data?
+
+# open real data
+# augment real data?
+
+# rescale and bottlenecks.  
+
+# train model
+    train in different ways
+
+# predict model
+
+@author: matthew
+"""
+
+
